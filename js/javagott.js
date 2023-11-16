40881558db12bb2d194be767b2c96a3693e8d2bc
Luis Teran <145719354+luigykun@users.noreply.github.com>
2023-11-15 21:21:36 -0300

    Slides

--- js/javagott.js
+++ js/javagott.js
@@ -115,6 +115,29 @@ let footer = `
 `
 document.getElementById("footer").innerHTML = footer
 
+
+//  Codigo para el SLIDE
+const slider = document.querySelector('.slides');
+let isSliding = false;
+
+function nextSlide() {
+    if (!isSliding) {
+        isSliding = true;
+        const firstSlide = document.querySelector('.slide:first-child');
+        slider.style.transition = 'transform 0.5s ease-in-out';
+        slider.style.transform = 'translateX(-100%)';
+        
+        setTimeout(() => {
+            slider.appendChild(firstSlide);
+            slider.style.transition = 'none';
+            slider.style.transform = 'translateX(0)';
+            isSliding = false;
+        }, 500);
+    }
+}
+
+setInterval(nextSlide, 4000); // Cambiar slide cada 4 segundos
+
 //-------------------------------Validacion de Formulario
 
 function validarEnviar() {
